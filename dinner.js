e2c2d1f5b731b1f32952bde43c398fb3fe454eb6
dotstry <dotstry@gmail.com>
2024-06-14 11:11:28 -0700

    created both test files

---
+++ dinner.js
@@ -0,0 +1,25 @@
+let bill = 10.25 + 3.99 + 7.15;
+let tip = bill*.15;
+let total = bill+tip;
+
+console.log(bill);
+console.log(tip);
+console.log("The Total is: " + "$"+ total.toFixed(2));
+
+console.log("\n");
+
+let adjective1 = "fun";
+let adjective2 = "exciting";
+let adjective3 =  "amazing";
+
+
+
+console.log("The Intro to JavaScript course is " + adjective1 + ". James and Julia are so " + adjective2 + ". I cannot wait to work through the rest of this " + adjective3 + " content!");
+
+console.log("\n");
+
+let firstName = "Dotstry";
+let interest = "chincillas";
+let hobby = "video games";
+
+console.log("Hi, my name is " + firstName + " I love " + interest + " In my spare time, I like to play " + hobby + ".");
---
+++ first-expressions.js
@@ -0,0 +1,33 @@
+// commenting out each section as it is no longer the active one
+// console.log((4+2)*7);
+
+/*  commenting out each section as it is no longer the active one
+const thingy = 1;
+const count = 1;
+const postLiked = false;
+const firstName = "Richard";
+*/
+
+const tempConversiontoF = "F=C×1.8+32"
+let celsius = 12;
+
+let fahrenheit = (celsius*1.8)+32;
+console.log(celsius + " degrees Celsius equals " + fahrenheit + " degrees Fahrenheit");
+
+console.log("\"I'll look down and whisper \"Try again later.\"\"");
+
+console.log("\"The file located at \"C:\\\\Desktop\\My Documents\\Roster\\names.txt\" contains the names on the roster.\"");
+
+console.log("green" > "blue");
+
+console.log("Q. Why couldn't the shoes go out and play?\nA. They were all \"tied\" up!");
+
+let stanza1 = "Blowing from the west"
+let stanza2 = "Fallen leaves gather"
+let stanza3 = "In the east"
+console.log("\n" + stanza1 + "\n" + stanza2 + "\n" + stanza3)
+
+let thingOne = "red";
+let thingTwo = "blue";
+
+console.log(thingOne+thingTwo);
